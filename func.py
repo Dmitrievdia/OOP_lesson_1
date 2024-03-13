e6ac68d26b7c6ec465e840a9b7b6de8f916e48ab
dia <zond86@mail.ru>
2024-03-13 12:53:06 +0300

    feat: fix code 13.2

--- func.py
+++ func.py
@@ -15,11 +15,17 @@ class Category:
 
     def add_products(self, product):
         """
-        функция добавления продукта в список
+        функция добавления продукта в список также данный метод считает итоговое количество всех продуктов в сумме
         :param product: str (Samsung s-23)
         :return: list ['Samsung s-23']
         """
-        self.__products.append(product)
+        for item in self.__products:
+            if item[0] == product.name:  # Проверяем наименование товара есть ли в списке продуктов
+                item[3] += product.quantity  # Обновляем количество товаров в продукте который уже есть
+                break
+            else:
+                self.__products.append([product.name, product.description, product.price, product.quantity])
+                Category.all_quantity_unique_product += 1
 
     @property
     def display(self):
@@ -29,17 +35,9 @@ class Category:
         """
         return self.__products
 
-    @property
-    def counting_products(self):
-        """
-        функция возвращает количество товаров
-        :return: int
-        """
-        return len(self.__products)
-
     def __repr__(self):
         """
-        функция возвращает список товаров в формате:
+        функция возвращает экземпляр категории в формате:
         Продукт, цена, остаток.
         :return: str
         """
@@ -48,7 +46,7 @@ class Category:
     @property
     def get_products(self):
         """
-        функция выводит список товаров в формате:
+        геттер-функция выводит список товаров в необходимом формате:
         Продукт, 80 руб. Остаток: 15 шт.
         :return:
         """
@@ -56,6 +54,14 @@ class Category:
         for product in self.__products:
             result += f'\n{product.name},{product.price}руб. Остаток: {product.quantity} шт.'
         return result
+
+    @property
+    def counting_products(self):
+        """
+        функция возвращает количество товаров на складе
+        :return: int
+        """
+        return len(self.__products)
 
 
 class Product:
@@ -72,9 +78,6 @@ class Product:
         self.quantity = quantity
         self.amount_available = quantity  # доступное количество
         Product.quantity_of_product += 1
-
-    def __repr__(self):
-        return f'{self.name}, {self.description}, {self.__price}, {self.quantity}'
 
     @classmethod
     def add_product(cls, new_product):
@@ -99,15 +102,57 @@ class Product:
         if new_price <= 0:
             print("Введена некорректная цена")
         else:
+            if self.__price > new_price:
+                input_user = input("Подтвердите понижение стоимости (y/n):\n").strip()
+                if input_user == "y":
+                    self.__price = new_price
+                else:
+                    print("Цена не изменилась")
             self.__price = new_price
 
+    def __repr__(self):
+        # Добавлено отображение результата
+        return f'{self.name}, {self.description}, {self.__price}, {self.quantity}'
 
-category = Category("Категория1", "qwerty")
-product = Product("Продукт1", "йцукен", 18000, 5)
-product1 = Product("Продукт2", "йцукен", 12000, 77)
-category.add_products(product1)
-category.add_products(product)
+    def __str__(self):
+        # Добавлено строковое отображение
+        return f'Название продукта: {self.name}, {self.__price} руб. Остаток: {self.quantity} шт.'
 
 
-print(product)
-print(product1)
+if __name__ == '__main__':
+    # Создание словаря для дальнейшего добавления в экземпляры класса
+    new_product_3 = {
+        'name': 'Nokia',
+        'description': 'smth',
+        'price': 1000,
+        'quantity': 10
+    }
+
+    product_3 = Product.add_product(new_product_3)  # Добавление нового продукта
+    print(product_3)  # Вывод добавленного словаря
+
+    product_1 = Product('Samsung', 'smth', 90_000, 2)
+    print(product_1)
+    product_2 = Product('iPhone', 'smth', 100_000, 3)  # Экземпляр класса Product
+    print(product_2)
+    # print(f'Метод add для 2х экземпляров класса Product: {product_1} + {product_2}')
+
+
+# работа с категориями и продуктами
+    category_1 = Category('Телефоны', 'мобильные телефоны')  # Экземпляр класса Category
+    product_1 = Product('Samsung', 'smth', 80_000, 8)  # Экземпляр класса Product
+    product_2 = Product('iPhone', 'smth', 130_000, 12)  # Экземпляр класса Product
+
+    category_1.add_products(product_1)  # Добавление продукта в приватный список товаров
+    category_1.add_products(product_2)  # Добавление продукта в приватный список товаров
+    print(category_1.add_products)
+
+    product_3 = Product('iPhone', 'smth', 130_000, 34)
+    category_1.add_products(product_3)  # Добавление продукта в приватный список товаров если такой продукт существует
+    print(category_1.add_products)  # Отображение приватного списка товаров
+
+    print(f'\nУникальных продуктов: {category_1.all_quantity_unique_product}')  # Кол-во уник-х товаров в прив. списке
+    print(category_1.get_products)  # Получение перечня товаров в определенном формате
+    print(str(category_1))  # Отображение строкового представления
+
+    print(f'Общее количество продуктов категории: {category_1.all_quantity_unique_product}')
